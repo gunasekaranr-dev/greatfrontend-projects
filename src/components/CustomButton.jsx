76d74764607d42ec355b972c3a761bc3bac7c115
Gunasekaran R <gunasekaranr2002@gmail.com>
2025-09-25 21:33:27 +0530

    button component completed

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -8,14 +8,14 @@ const CustomButton = () => {
     return (
         <div className='position-relative vh-100'>
             <div className='w-75 position-absolute top-50 start-50 translate-middle'>
-                {CustomButtonContent.map(btn => (
+                {CustomButtonContent.map((btn, index) => (
                     btn.rows.map((row, rowIndex) => (
-                        <Button key={rowIndex} variant={row.varient} size={row.size} style={{ backgroundColor: row.colorCode }} className={`text-${row.text} border-0 rounded-1 mx-5 my-1 p-2`}>
+                        <Button key={rowIndex} variant={row.varient} size={row.size} style={{ backgroundColor: row.backgroundColorCode, color: row.textColorCode }} className='w-auto border-0 rounded-1 mx-5 my-1 p-2'>
                             <span className='d-flex align-items-center'>
-                                {row.isFirstIcon && <CiStar className='me-2 p-0'/>}
+                                {row.isFirstIcon && <CiStar className='me-2 p-0' />}
                                 {row.content}
-                                {row.isLastIcon && <CiStar style={{ marginLeft: '5px'}}/>}
-                                {row.icon && <CiStar className='p-0 m-0'/>}
+                                {row.isLastIcon && <CiStar style={{ marginLeft: '5px' }} />}
+                                {row.icon && <CiStar className='p-0 m-0' />}
                             </span>
                         </Button>
                     ))
